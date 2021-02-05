e5ae26cbc00ae3309adf25cb2be089f559a86172
mikansc <michael.nsc@outlook.com>
2021-02-05 14:53:19 -0300

    add dinamic photo

--- src/pages/home/StudentCard.jsx
+++ src/pages/home/StudentCard.jsx
@@ -4,22 +4,27 @@ import { ReactComponent as GithubLogo } from "../../assets/img/github-square-bra
 const StudentCard = ({ student, onClick }) => {
   return (
     <div className="card" onClick={onClick && onClick}>
-      <div className="card-data">
-        <h3>{student.nome}</h3>
-        <p>{student.idade} anos</p>
+      <div className="column-left">
+        <img src={`${student.foto?.url}`} alt="Imagem" />
       </div>
-      <div className="socials">
-        <a href={student.linkedin} target="_blank" rel="noreferrer">
-          <LinkedInLogo
-            color="#0077B5"
-            alt="LinkedIn Logo"
-            width="25"
-            height="25"
-          />
-        </a>
-        <a href={student.github} target="_blank" rel="noreferrer">
-          <GithubLogo color="#000" alt="Github Logo" width="25" height="25" />
-        </a>
+      <div className="column-right">
+        <div className="card-data">
+          <h3>{student.nome}</h3>
+          <p>{student.idade} anos</p>
+        </div>
+        <div className="socials">
+          <a href={student.linkedin} target="_blank" rel="noreferrer">
+            <LinkedInLogo
+              color="#0077B5"
+              alt="LinkedIn Logo"
+              width="25"
+              height="25"
+            />
+          </a>
+          <a href={student.github} target="_blank" rel="noreferrer">
+            <GithubLogo color="#000" alt="Github Logo" width="25" height="25" />
+          </a>
+        </div>
       </div>
     </div>
   );
